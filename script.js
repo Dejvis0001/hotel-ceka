3747ad11aeb17b0b3bef5765cca8ef2b94ce2125
Dejvis0001 <devimyrto26@outlook.com>
2024-03-25 22:04:17 +0100

    1st night

--- script.js
+++ script.js
@@ -39,29 +39,4 @@ document.addEventListener("DOMContentLoaded", function () {
   carousel.addEventListener("slid.bs.carousel", function () {
     setControlBackgrounds();
   });
-});const btns = document.querySelectorAll(".btn");
-const slideRow = document.getElementById("slide-row");
-const main = document.querySelector("main");
-
-let currentIndex = 0;
-
-function updateSlide() {
-  const mainWidth = main.offsetWidth;
-  const translateValue = currentIndex * -mainWidth;
-  slideRow.style.transform = `translateX(${translateValue}px)`;
-
-  btns.forEach((btn, index) => {
-    btn.classList.toggle("active", index === currentIndex);
-  });
-}
-
-btns.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
-    currentIndex = index;
-    updateSlide();
-  });
-});
-
-window.addEventListener("resize", () => {
-  updateSlide();
 });
