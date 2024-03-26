e1d14d09952c7a107030948b54a5513e4c03181d
Dejvis0001 <devimyrto26@outlook.com>
2024-03-26 18:03:17 +0100

    2nd day of work

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const navEl = document.querySelector(".navbar");
-
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 56) {
     navEl.classList.add("navbar-scrolled");
@@ -11,12 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
   var carouselImages = document.querySelectorAll(".carousel-item img");
   var prevControl = document.querySelector(".carousel-control-prev");
   var nextControl = document.querySelector(".carousel-control-next");
-
   function setControlBackgrounds() {
     var activeIndex = Array.from(carouselImages).findIndex(function (img) {
       return img.parentElement.classList.contains("active");
     });
-
     if (activeIndex === 0) {
       prevControl.style.backgroundImage =
         "url(" + carouselImages[carouselImages.length - 1].src + ")";
@@ -24,7 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
       prevControl.style.backgroundImage =
         "url(" + carouselImages[activeIndex - 1].src + ")";
     }
-
     if (activeIndex === carouselImages.length - 1) {
       nextControl.style.backgroundImage = "url(" + carouselImages[0].src + ")";
     } else {
@@ -32,11 +28,22 @@ document.addEventListener("DOMContentLoaded", function () {
         "url(" + carouselImages[activeIndex + 1].src + ")";
     }
   }
-
   setControlBackgrounds();
-
   var carousel = document.querySelector(".carousel");
   carousel.addEventListener("slid.bs.carousel", function () {
     setControlBackgrounds();
   });
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
