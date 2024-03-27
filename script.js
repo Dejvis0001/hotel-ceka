45356da57ef0c45506a7d21a444b30a26cabddb0
Dejvis0001 <devimyrto26@outlook.com>
2024-03-27 17:57:09 +0100

    3rd day of work , finished index ,contact and created gallery

--- script.js
+++ script.js
@@ -7,31 +7,71 @@ window.addEventListener("scroll", () => {
   }
 });
 document.addEventListener("DOMContentLoaded", function () {
-  var carouselImages = document.querySelectorAll(".carousel-item img");
-  var prevControl = document.querySelector(".carousel-control-prev");
-  var nextControl = document.querySelector(".carousel-control-next");
-  function setControlBackgrounds() {
-    var activeIndex = Array.from(carouselImages).findIndex(function (img) {
+  var carouselImages1 = document.querySelectorAll(
+    "#CarouselHead .carousel-item img"
+  );
+  var prevControl1 = document.querySelector(
+    "#CarouselHead .carousel-control-prev"
+  );
+  var nextControl1 = document.querySelector(
+    "#CarouselHead .carousel-control-next"
+  );
+  var carouselImages2 = document.querySelectorAll(
+    "#CarouselRoom .carousel-item img"
+  );
+  var prevControl2 = document.querySelector(
+    "#CarouselRoom .carousel-control-prev"
+  );
+  var nextControl2 = document.querySelector(
+    "#CarouselRoom .carousel-control-next"
+  );
+  function setControlBackgrounds1() {
+    var activeIndex = Array.from(carouselImages1).findIndex(function (img) {
       return img.parentElement.classList.contains("active");
     });
     if (activeIndex === 0) {
-      prevControl.style.backgroundImage =
-        "url(" + carouselImages[carouselImages.length - 1].src + ")";
+      prevControl1.style.backgroundImage =
+        "url(" + carouselImages1[carouselImages1.length - 1].src + ")";
     } else {
-      prevControl.style.backgroundImage =
-        "url(" + carouselImages[activeIndex - 1].src + ")";
+      prevControl1.style.backgroundImage =
+        "url(" + carouselImages1[activeIndex - 1].src + ")";
     }
-    if (activeIndex === carouselImages.length - 1) {
-      nextControl.style.backgroundImage = "url(" + carouselImages[0].src + ")";
+    if (activeIndex === carouselImages1.length - 1) {
+      nextControl1.style.backgroundImage =
+        "url(" + carouselImages1[0].src + ")";
     } else {
-      nextControl.style.backgroundImage =
-        "url(" + carouselImages[activeIndex + 1].src + ")";
+      nextControl1.style.backgroundImage =
+        "url(" + carouselImages1[activeIndex + 1].src + ")";
     }
   }
-  setControlBackgrounds();
-  var carousel = document.querySelector(".carousel");
-  carousel.addEventListener("slid.bs.carousel", function () {
-    setControlBackgrounds();
+  function setControlBackgrounds2() {
+    var activeIndex = Array.from(carouselImages2).findIndex(function (img) {
+      return img.parentElement.classList.contains("active");
+    });
+    if (activeIndex === 0) {
+      prevControl2.style.backgroundImage =
+        "url(" + carouselImages2[carouselImages2.length - 1].src + ")";
+    } else {
+      prevControl2.style.backgroundImage =
+        "url(" + carouselImages2[activeIndex - 1].src + ")";
+    }
+    if (activeIndex === carouselImages2.length - 1) {
+      nextControl2.style.backgroundImage =
+        "url(" + carouselImages2[0].src + ")";
+    } else {
+      nextControl2.style.backgroundImage =
+        "url(" + carouselImages2[activeIndex + 1].src + ")";
+    }
+  }
+  setControlBackgrounds1();
+  setControlBackgrounds2();
+  var carousel1 = document.querySelector("#CarouselHead");
+  var carousel2 = document.querySelector("#CarouselRoom");
+  carousel1.addEventListener("slid.bs.carousel", function () {
+    setControlBackgrounds1();
+  });
+  carousel2.addEventListener("slid.bs.carousel", function () {
+    setControlBackgrounds2();
   });
 });
 var swiper = new Swiper(".mySwiper", {
